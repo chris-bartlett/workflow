4be6cb73c6dbe859661932e2471557157ed4cb70
chrisbartletthome <cbartlett@riskwaters.com>
2017-02-01 23:10:27 +0800

    added for js and css minification

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,17 @@ var browserify = require('gulp-browserify');
 var merge = require('merge-stream');
 var newer = require('gulp-newer');
 var imagemin = require('gulp-imagemin');
+var injectPartials = require('gulp-inject-partials');
+var minify = require('gulp-minify');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 var reload = browserSync.reload;
 
 //setup sourcepaths and app path so don't need to keep typing them
 var SOURCEPATHS = {
   sassSource: 'src/scss/*.scss',
   htmlSource: 'src/*.html',
+  htmlPartialSource: 'src/partial/*.html',
   jsSource: 'src/js/**',
   imgSource: 'src/img/**'
 }
@@ -27,6 +32,35 @@ var APPPATH = {
   fonts: 'app/fonts',
   img: 'app/img'
 }
+
+
+/*  START PRODUCTION TASKS */
+
+//to run: gulp compress - used as separate task not every time
+gulp.task('compress', function() {
+  return gulp.src(SOURCEPATHS.jsSource)
+    .pipe(concat('main.js'))
+    .pipe(browserify())
+    .pipe(minify())
+    .pipe(gulp.dest(APPPATH.js));
+});
+
+//to run: gulp compresscss - used as separate task not every time
+gulp.task('compresscss', function() {
+  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+  var sassFiles;
+  sassFiles = gulp.src(SOURCEPATHS.sassSource)
+    .pipe(autoprefixer())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+
+    return merge(sassFiles, bootstrapCSS)
+      .pipe(concat('app.css'))
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest(APPPATH.css));
+});
+
+/*  END PRODUCTION TASKS */
 
 //copy js files and delete them if deleted from source, also conact them into one and include the required files install via npm
 gulp.task('scripts', ['clean-scripts'], function() {
@@ -46,6 +80,13 @@ gulp.task('clean-html', function() {
 gulp.task('clean-scripts', function() {
   return gulp.src(APPPATH.js + '/*.js', {read: false, force: true})
     .pipe(clean());
+});
+
+//inject partials into html
+gulp.task('html', function() {
+  return gulp.src(SOURCEPATHS.htmlSource)
+    .pipe(injectPartials())
+    .pipe(gulp.dest(APPPATH.root));
 });
 
 //setup sass task to conver scss to css and autoprefixer will add -webkit-(etc) prefixes for CSS3 where needed!
@@ -77,11 +118,11 @@ gulp.task('moveFonts', function() {
     .pipe(gulp.dest(APPPATH.fonts));
 })
 
-//copy any html files from source to app and delete them if deleted from source
-gulp.task('copy', ['clean-html'], function() {
-  gulp.src(SOURCEPATHS.htmlSource)
-    .pipe(gulp.dest(APPPATH.root))
-});
+// //copy any html files from source to app and delete them if deleted from source
+// gulp.task('copy', ['clean-html'], function() {
+//   gulp.src(SOURCEPATHS.htmlSource)
+//     .pipe(gulp.dest(APPPATH.root))
+// });
 
 //setup serve task to launch server as soon as run gulp
 gulp.task('serve', ['sass'], function() {
@@ -93,10 +134,11 @@ gulp.task('serve', ['sass'], function() {
 });
 
 //setup watch task to update the server if any sass files change - server will automatically update html changes
-gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts','images'], function() {
+gulp.task('watch', ['serve', 'sass', 'html', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts','images'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
-  gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+  //gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
   gulp.watch([SOURCEPATHS.jsSource], ['scripts']);  
+  gulp.watch([SOURCEPATHS.htmlSource, SOURCEPATHS.htmlPartialSource], ['html']); 
 
 });
 
